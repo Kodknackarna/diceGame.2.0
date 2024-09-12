80d37fbfbc03f51530bc951e07dd77fbf915d2f7
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-09-12 14:54:12 +0200

    Deleted some unneccesary comments

--- diceGame1/main.cpp
+++ diceGame1/main.cpp
@@ -9,10 +9,6 @@
 int main() {
 
     srand(time(0)); // Initialisera slumptalsgeneratorn med nuvarande tid
-    //testkommentar
-    //Dice dice(6, 0);
-
-    //dice.roll();
 
     std::vector<Dice> diceVector = { Dice(6, 0) };
     std::vector<Player*> players;
@@ -44,7 +40,6 @@ int main() {
         std::cout << "The winner is: " << std::endl;
     };
 
-    
     for (const auto& winner : winners) {
         std::cout << winner << std::endl;
 
@@ -52,30 +47,6 @@ int main() {
     for (auto player : players) {
         delete player;
     }
-   
-    ////Player player1("Adam", 0, diceVector);
-    ////Player player2("Youssef", 0, diceVector);
-
-
-    //std::cout << "Player 1, first roll with 1 die: " << player1.rollDice() << std::endl;
-    //std::cout << "Player 2, first roll with 1 die: " << player2.rollDice() << std::endl;
-
-    //player1.addDie(6);
-    //player2.addDie(6);
-
-    //player1.rollDice();
-    //player2.rollDice();
-
-    //std::cout << "Player 1, second roll with 2 dice: " << player1.getTotalValue() << std::endl;
-    //std::cout << "Player 2, second roll with 2 dice: " << player2.getTotalValue() << std::endl;
-
-    //player1.increaseScore();
-
-    //std::cout << player1.getScore() << std::endl;
-
-    //player1.increaseScore();
-
-    //std::cout << player1.getScore() << std::endl;
 
     return 0;
 }
